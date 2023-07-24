b1a59ddded97a048143221e9634717caceea7a17
sun-8 <rhdqnwjsdyd0@naver.com>
2023-07-24 18:06:41 +0900

    git 테스트01

---
+++ src/test/java/GithubTest.java
@@ -0,0 +1,10 @@
+public class GithubTest {
+    // 이것은 git 테스트 파일입니다.
+    // feature을 하나 생성 합니다. 이름은 test01. 파일을 수정하여 commit을 합니다.
+    // 또 하나의 feature을 생성하되 이름은 test01_01로 하고, feature/test01을 기준으로 feature를 생성합니다.
+    // test01_01에도 수정사항을 적용하여 commit합니다.
+    // 그렇다면 이런 형식일 것입니다.
+    /* feature/test01
+            feature/test01_01 */
+    // 이때, test01을 push하여 develop에 merge시켰을 때 test01_01은 어떠한 상태가 되는지 테스트 해보겠습니다.
+}
