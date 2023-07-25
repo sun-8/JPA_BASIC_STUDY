7206ca5183eaebd82973330130f87e1e21257fae
sun-8 <rhdqnwjsdyd0@naver.com>
2023-07-25 16:42:41 +0900

    git 테스트01 결과

--- src/test/java/GithubTest.java
+++ src/test/java/GithubTest.java
@@ -9,4 +9,15 @@ public class GithubTest {
     // 이때, test01을 push하여 develop에 merge시켰을 때 test01_01은 어떠한 상태가 되는지 테스트 해보겠습니다.
     
     // feature/test01_01을 생성했습니다.
+
+    // 실험 결과 : test01 feature를 finish하여 develop에 merge 후
+    //      develop push한 상태에서 test01_01 feature를 finish 하려고 했지만
+    //      'Error: Please have a look at the Version Control console for more details'라는 에러메시지를 띄우며 실행되지 않았습니다.
+    //      feature/test01_01 에서 다음 명령을 실행하여 develop 브랜치와 rebase를 진행했습니다.
+    //      'git rebase develop'
+    //      해당 명렁어로 인해 feature/test01_01 브랜치가 develop 브랜치 위로 rebase되었습니다.
+    //      이후에는 필요에 따라 develop 브랜치를 push하여 원격 저장소와 동기화하면 됩니다.
+    //      기존에 있던 feature는 delete해주었습니다.
+
+    // ! 해당 실험이 여러가지 시도를 해볼 결과라서 실험결과가 정확하지 않을 수 있습니다.
 }
